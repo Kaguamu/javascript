271b65a95e334972a1e4004fd35a9da923b0f7e0
Kaguamu <duncanmutiga7@gmail.com>
2025-08-23 12:51:23 +0300

    Add files via upload

---
+++ JAVASCRIPT/tempconverter/Dice-Roll/index3.js
@@ -0,0 +1,15 @@
+
+function rollDice(){
+const input=document.getElementById("input").value;
+const diceresult=document.getElementById("diceresult");
+const images=document.getElementById("images");
+
+const value = [];
+const image = [];
+
+for(let i=0;i<input;i++){
+    const num=Math.floor(Math.random()*6)+1;
+    }
+    console.log(num);
+
+}
