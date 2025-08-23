669f0ccb1d76fe6285e0573d9e9bc8996429c242
Kaguamu <duncanmutiga7@gmail.com>
2025-08-23 17:34:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const num5=5;
 let numbers=sunNum(num1,num2,num3,num4);
 console.log(...numbers);*/
 
-/*function to combine strings -rest  parameter
+//function to combine strings -rest  parameter
 function combineStrings(...string){
     return string.join("");
 }
@@ -101,4 +101,117 @@ function sum(callback,x,y){
 function displayConsole(result){
     console.log(result);
 }
+//simple grading using switch case
+let grade=80;
+let testscore= Math.floor(grade/10);
+ console.log(testscore);
+
+
+switch(testscore){
+    case 10:
+        console.log('A');
+        break;
+    case 9:
+        console.log('B');
+        break;
+    case 8:
+        console.log('C');
+        break;
+    case 7:
+        console.log('D');
+        break;
+    case 6:
+        console.log('E');
+        break;
+    case 5:
+        console.log('F');
+        break;
+    case 4:
+        console.log('Z');
+        break;
+    default:
+        console.log("invalid grade");
+}
+
+
+//prompt the user using window.prompt ask for their name then trim it and capitalize the firt letter
+let username=window.prompt("Enter you name ");
+username=username.trim();
+let firstchar = username.charAt(0);
+firstchar=firstchar.toUpperCase();
+let letters= username.slice(1);
+username = firstchar + letters;
+
+
+console.log(`you name is ${username}`);
+*/
+/*
+let username=window.prompt("Enter your name: ");
+
+do{
+    username=window.prompt("Enter name: ");
+}
+while(username=== "" || username===null){
+}
+console.log(`Hello  ${username}`);*/
+/*function to generate a randoom number
+function generate(){
+    let num=Math.floor(Math.random()*6)+1;
+    return num;
+}
+
+let number=generate();
+console.log(number);*/
+
+
+//function to check if a number is even or odd
+function is_even(x){
+    if(x%2==0){
+    return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let answer=is_even(10);
+console.log(answer);
+
+
+
+
+//forEach() change to lowercase
+let fruits=["APPLE","BANANA","MANGO","COCONUT"];
+fruits.forEach(lowerCase);
+fruits.forEach(display);
+
+function display(element){
+    console.log(element);
+}
+function upperCase(element,index,array){
+    array[index] = element.toUpperCase();
+}
+
+function lowerCase (element,index,array){
+    array[index] = element.toLowerCase();
+}
+//forEach method
+let numbers =[1,2,3,4,5];
+numbers.forEach(triple);
+numbers.forEach(display);
+
+
+function double(element){
+    array[index]=element * 2;
+}
+function triple(element,index,array){
+    let ans=element*element*element;
+    console.log(ans);
+}
+function display(element){
+    console.log(element);
+}
+
+
+
 
