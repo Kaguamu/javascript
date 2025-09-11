f2f12feb9525ae1e4bdd7149ab7354ddc512a5ba
Kaguamu <duncanmutiga7@gmail.com>
2025-09-11 09:21:53 +0300

    Add files via upload

---
+++ prac8.js
@@ -0,0 +1,31 @@
+/*let fruits=["orange","banana","apple","mango","coconut"];
+fruits.sort();
+console.log(fruits);
+
+let number=[7,5,4,3,8,9,10,1,2]
+number.sort();
+console.log(number);
+//sorting lexicographically
+let numbers=[7,5,4,3,8,9,10,1,2]
+numbers.sort((a, b) => b - a);
+console.log(numbers);
+
+//sorting people by age
+let people=[{name:"bro",age:40,gpa:4.0},
+            {name:"code",age:30,gpa:2.0},
+            {name:"spongebob",age:37,gpa:3.5}];
+people.sort((a,b)=>a.age-b.age);
+console.log(people);
+//sorting by gpa
+let persons=[{name:"bro",age:40,gpa:4.0.toFixed(1)},
+            {name:"code",age:30,gpa:2.0.toFixed(1)},
+            {name:"spongebob",age:37,gpa:3.5}];
+persons.sort((a,b)=>a.gpa-b.gpa);
+console.log(persons);
+
+//sorting by name
+let person=[{name:"spongebob",age:37,gpa:3.5},
+            {name:"bro",age:40,gpa:4.0.toFixed(1)},
+            {name:"code",age:30,gpa:2.0.toFixed(1)}];
+person.sort((a,b)=> a.name.localeCompare(b.name));
+console.log(person);*/
