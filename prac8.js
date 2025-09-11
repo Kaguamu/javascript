3763d6e48c0c27b9bbd165eb06031daf81a14253
Kaguamu <duncanmutiga7@gmail.com>
2025-09-11 09:23:13 +0300

    Update prac8.js

--- prac8.js
+++ prac8.js
@@ -1,10 +1,13 @@
-/*let fruits=["orange","banana","apple","mango","coconut"];
+//sorting an array with strings 
+let fruits=["orange","banana","apple","mango","coconut"];
 fruits.sort();
 console.log(fruits);
 
+//sorting numbers
 let number=[7,5,4,3,8,9,10,1,2]
 number.sort();
 console.log(number);
+
 //sorting lexicographically
 let numbers=[7,5,4,3,8,9,10,1,2]
 numbers.sort((a, b) => b - a);
@@ -16,6 +19,7 @@ let people=[{name:"bro",age:40,gpa:4.0},
             {name:"spongebob",age:37,gpa:3.5}];
 people.sort((a,b)=>a.age-b.age);
 console.log(people);
+
 //sorting by gpa
 let persons=[{name:"bro",age:40,gpa:4.0.toFixed(1)},
             {name:"code",age:30,gpa:2.0.toFixed(1)},
@@ -28,4 +32,5 @@ let person=[{name:"spongebob",age:37,gpa:3.5},
             {name:"bro",age:40,gpa:4.0.toFixed(1)},
             {name:"code",age:30,gpa:2.0.toFixed(1)}];
 person.sort((a,b)=> a.name.localeCompare(b.name));
-console.log(person);*/
+
+console.log(person);
